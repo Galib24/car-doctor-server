8c4de7560f7eb7df04b517f62ac17311840f279c
SQLAdmin <abuyeahia24@gmail.com>
2023-05-14 12:15:30 +0600

    fixe somethings

--- index.js
+++ index.js
@@ -71,6 +71,22 @@ async function run() {
             res.send(result);
         });
 
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateBooking = req.body;
+            console.log(updateBooking);
+            const updateDoc = {
+                $set: {
+                    status: updateBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter,updateDoc);
+            res.send(result);
+        })
+
+
+
         app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
