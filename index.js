db13644c7f410162527b209683e8282580e30632
SQLAdmin <abuyeahia24@gmail.com>
2023-05-16 11:50:27 +0600

    added to vercel

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const verifyJWT = (req, res, next)=>{
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const serviceCollection = client.db('carDoctors').collection('services');
